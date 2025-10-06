9de7b9fa39f9b9b4b941d82f444df76f187c466f
Rushikesh Gajanan Salunkhe <rushikeshsalunkhe70@gmail.com>
2025-10-06 20:05:59 +0530

    Update BatchEntry.java

--- src/main/java/com/marvellous/marvellousportal/Entity/BatchEntry.java
+++ src/main/java/com/marvellous/marvellousportal/Entity/BatchEntry.java
@@ -1,48 +1,16 @@
-package com.marvellous.marvellousportal.Entity;
+package com.marvellous.MarvellousPortal.Entiy;
 
-import org.apache.tomcat.util.net.openssl.OpenSSLStatus;
+import lombok.*;
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
-
 @Document(collection = "BatchDetails")
+
+@Data
 public class BatchEntry
 {
-    @Id
     private ObjectId id;
     private String name;
     private int fees;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ObjectId getId()
-    {
-        return id;
-    }
-
-    public void setId(ObjectId id)
-    {
-        this.id = id;
-    }
-
-    public int getFees()
-    {
-        return fees;
-    }
-
-    public void setFees(BatchEntry myentry, int fees) {
-        this.fees = fees;
-    }
-
-    public void setName(BatchEntry myentry, OpenSSLStatus.Name name) {
-        
-    }
 }
